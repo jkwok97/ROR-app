1914e17813d4edffb29d9c7e8c1a6329e24c17b0
Jeff Kwok <jkwok97@gmail.com>
2019-10-09 13:37:02 -0600

    numbers in ruby

---
+++ Ruby Lang/numbers/arithmetic.rb
@@ -0,0 +1,15 @@
+# addition
+p 5 + 5
+
+# subtraction
+p 5 - 5
+
+# division
+p 5 / 5
+
+# multiplication
+p 5 * 5
+
+# exponents
+p 5**5
+
---
+++ Ruby Lang/numbers/order_of_operations.rb
@@ -0,0 +1,18 @@
+# PEMDAS
+# PLEASE EXCUSE MY DEAR AUNT SALLY
+
+# P - Parenthesis
+# E - Exponents
+# M - Mulitplication
+# D - Division
+# A - Addition
+# S - Subtraction
+
+p 5 + 15 * 20 - 2 / 6**3 - (3 + 1)
+# 5 + 15 * 20 - 2 / 6**3 - 4
+# 5 + 15 * 20 - 2 / 216 - 4
+# 5 + 15 * 20 - 0 - 4
+# 5 + 300 - 0 - 4
+# 305 - 0 - 4
+#  => 301
+
