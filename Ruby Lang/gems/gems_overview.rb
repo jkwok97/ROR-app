ebcc026a037f681c3ce1e856b62d6c8cf15c7ab4
Jeff Kwok <jkwok97@gmail.com>
2019-10-16 14:22:46 -0600

    example of using ruby gems with dish

---
+++ Ruby Lang/gems/gems_overview.rb
@@ -0,0 +1,18 @@
+require 'rubygems'
+require 'dish'
+
+hash = {
+    movie: "Star Wars",
+    actors: [
+        { age: 22, name: "Luke" },
+        { age: 35, name: "Han" },
+        { age: 400, name: "Yoda" }
+    ],
+    favorite: true
+}
+
+p media = Dish(hash)
+p media.movie
+p media.actors.length
+p media.actors[0].name
+p media.favorite?
