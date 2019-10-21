5e7874454fdc5b1e861c8bcec2a46ba6765296c2
Jeff Kwok <jkwok97@gmail.com>
2019-10-21 11:08:53 -0600

    added edit and update functionality for showcase

--- Portfolio/app/controllers/showcases_controller.rb
+++ Portfolio/app/controllers/showcases_controller.rb
@@ -21,5 +21,22 @@ class ShowcasesController < ApplicationController
         end
     end
 
+    def edit
+        @showcase_item = Showcase.find(params[:id])
+    end
+
+    def update
+        @showcase_item = Showcase.find(params[:id])
+        
+        respond_to do |format|
+          if @showcase_item.update(params.require(:showcase).permit(:title, :subtitle, :body))
+            format.html { redirect_to showcases_path, notice: 'ShowCase Item was successfully updated.' }
+            format.json { render :show, status: :ok, location: @showcase_item }
+          else
+            format.html { render :edit }
+            format.json { render json: @showcase_item.errors, status: :unprocessable_entity }
+          end
+        end
+    end
 
 end
