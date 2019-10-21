085bc928bdcfb8344db7feb0cde0584ede4eac83
Jeff Kwok <jkwok97@gmail.com>
2019-10-21 10:55:35 -0600

    added new and create feature for showcase

--- Portfolio/app/controllers/showcases_controller.rb
+++ Portfolio/app/controllers/showcases_controller.rb
@@ -3,4 +3,23 @@ class ShowcasesController < ApplicationController
         @showcase_items = Showcase.all
     end
 
+    def new
+        @showcase_item = Showcase.new
+    end
+
+    def create
+        @showcase_item = Showcase.new(params.require(:showcase).permit(:title, :subtitle, :body))
+    
+        respond_to do |format|
+          if @showcase_item.save
+            format.html { redirect_to showcases_path, notice: 'ShowCase Item was successfully created.' }
+            format.json { render :show, status: :created, location: @showcase_item }
+          else
+            format.html { render :new }
+            format.json { render json: @showcase_item.errors, status: :unprocessable_entity }
+          end
+        end
+    end
+
+
 end
